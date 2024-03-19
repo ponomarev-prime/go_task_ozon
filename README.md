b770df4bedc6e2cd3c4ddc42cbb93e97d8215d55
alex <ax.dev@yahoo.com>
2024-03-19 21:14:00 +0300

    Closure

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ go
 ```
 docker run -it --rm -v .\:/app golang:1.22 go run /app/main.go
 ```
+
+# git
+```
+git config --global user.name "alex"
+git config --global user.email "ax.dev@yahoo.com"
+```
--- main.go
+++ main.go
@@ -9,10 +9,10 @@ func main() {
 	wg := sync.WaitGroup{}
 
 	wg.Add(5)
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 5; i++ { // Последнее значение 5
 		go func() {
-			fmt.Println(i)
-			wg.Done()
+			defer wg.Done() // Замыкание, wg прокидывается по указателю
+			fmt.Println(i)  // i прокидывается по указателю
 		}()
 	}
 
